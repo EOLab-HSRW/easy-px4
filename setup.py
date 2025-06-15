9bfbea4008e5e1c6447d7b73e77cc28a15dae1a5
harleylara <harley.lara@outlook.com>
2025-06-15 13:49:58 +0200

    update test versions

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    python_requires='>=3.10',
+    python_requires='>=3.8',
     install_requires=[
         'tomli',
     ],
